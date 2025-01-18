cb258efd23c0e47fbf1836e308f429c6052e523d
Tristan Wakefield <tristan.wakefield@yahoo.com>
2025-01-18 15:58:45 -0600

    partial

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const logger = require('morgan')
 const indexRoutes = require('/')
-const gameRoutes = require('/api/game')
+const gameRoutes = require('./gameRoutes')
 
 const app = express()
 app.use(logger('dev'))
 app.use(express.json())
-app.use(gameRoutes)
-app.use(indexRoutes)
+app.use('/api/game',gameRoutes)
+app.use('/',indexRoutes)
 
 app.listen(3000, ()=>{
     console.log("Server started.")
